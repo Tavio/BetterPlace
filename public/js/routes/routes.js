596780fb1d6cd9306d6a54f5c16ed26815e59dfe
Tavio <otanasc@gmail.com>
2015-02-06 22:41:39 -0200

    including google maps api v3

--- public/js/routes/routes.js
+++ public/js/routes/routes.js
@@ -1,15 +1,21 @@
 /**
  * Created by philipe on 06/02/15.
  */
+    console.log('routes');
 var BP = Backbone.Router.extend({
 
     routes: {
-        "/": "index"
+        "/": "index",
+        "/test": "test"
     },
 
     index: function() {
-
+        console.log('hahaha');
+    },
+    test: function(){
+        console.log('blah')
     }
 
 
 });
+new BP();
